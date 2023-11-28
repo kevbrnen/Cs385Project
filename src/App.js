59b333a50f9632e11f3e27a123a33df8a3537791
Kevin Brennan <147145956+kevbrnen@users.noreply.github.com>
2023-11-28 15:20:38 +0000

    Update tasks.json and App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./styles.css";
-import React, { useState, useRef } from "react";
-import soundFiles from "./jsonFiles/soundFiles.json";
+import React, { useState, useRef, useEffect } from "react";
+//import soundFiles from "./jsonFiles/soundFiles.json";
 
 export default function App() {
   // Screen changing code
@@ -15,60 +15,140 @@ export default function App() {
   };
 
   // Audio Loading and Playing Code
-  const audioSource =
-    "https://github.com/kevbrnen/Cs385Project/raw/main/AudioFiles/groove.mp3";
+  const [audioUrl, setAudioUrl] = useState("");
+  const [audioLoaded, setAudioLoaded] = useState(false); //keeps track of wether audio has been loaded from URL
   const audioRef = useRef(null);
+
+  // Home Screen
+  if (ScreenState === 0) {
+    //Code For Home Screen
+    return (
+      <div className="App">
+        <h1> Main Screen </h1>
+
+        <button onClick={ChangeScreen}>Search Screen</button>
+      </div>
+    );
+  } else if (ScreenState === 1) {
+    //Code for Search Screen
+    //Calls children "ResultsComponent" (for displaying json file)
+    //And "AudioPlayer" (for loading audio player and handling playback)
+    return (
+      <div className="App">
+        <h1> Search Screen </h1>
+
+        <ResultsComponent
+          setAudioUrl={setAudioUrl}
+          setAudioLoaded={setAudioLoaded}
+        />
+        <></>
+        <AudioPlayer
+          audioUrl={audioUrl}
+          audioRef={audioRef}
+          audioLoaded={audioLoaded}
+          setAudioLoaded={setAudioLoaded}
+        />
+
+        <button onClick={ChangeScreen}>Main Screen</button>
+      </div>
+    );
+  }
+}
+
+/***** Playback only works so far by selecting the groove audio file, changing to 
+      the main Screen, changing back to the search screen then pressing play *****/
+//Function to handle displaying audio player and audio playback
+function AudioPlayer(props) {
+  //Audio Playback state
   const [isPlaying, setIsPlaying] = useState(false);
 
+  //handles button press to pause and play audio
   const playAudio = () => {
-    if (audioRef.current) {
+    if (props.audioRef.current) {
       if (isPlaying) {
-        audioRef.current.pause();
+        props.audioRef.current.pause();
       } else {
-        audioRef.current.play();
+        props.audioRef.current.play();
       }
 
       setIsPlaying(!isPlaying);
     }
   };
 
-  // Play Screen
-  if (ScreenState === 0) {
-    return (
-      <div className="App">
-        <h1> Screen 1 </h1>
-        <ResultsComponent arrayFromParent={soundFiles} />
+  //checking if audio has loaded from URL
+  const checkAudioLoad = () => {
+    props.setAudioLoaded(true);
+  };
 
-        <button onClick={playAudio}>{isPlaying ? "Pause" : "Play"}</button>
+  //Audio Player Component Display
+  return (
+    <>
+      <audio ref={props.audioRef} onLoadedData={checkAudioLoad}>
+        <source src={props.audioUrl} type="audio/mp3" />
+      </audio>
 
-        {isPlaying && <p> Playing song </p>}
-        {!isPlaying && <p> not playing </p>}
+      {props.audioLoaded ? (
+        <p>Audio has been loaded!</p>
+      ) : (
+        <p>Loading audio... Please wait.</p>
+      )}
 
-        <audio ref={audioRef}>
-          <source src={audioSource} type="audio/mp3" />
-        </audio>
+      <button onClick={playAudio}>{isPlaying ? "Pause" : "Play"}</button>
 
-        <button onClick={ChangeScreen}>Change Screen</button>
-      </div>
-    );
-  } else if (ScreenState === 1) {
-    // Second Screen
-    return (
-      <div className="App">
-        <h1> Screen 2 </h1>
-        <button onClick={ChangeScreen}>Change Screen</button>
-      </div>
-    );
-  }
+      {isPlaying && <p> Playing song </p>}
+      {!isPlaying && <p> not playing </p>}
+    </>
+  );
 }
 
+//Function to Display audio files in JSON file
 function ResultsComponent(props) {
-  return;
-  <>
-    props.arrayFromParent.map((a,index) => (
-    <p key={index}>
-      <b>{a.title}</b>, <i>{a.environment.location}</i>
-    </p>
-    ))
-  </>;
+  //Code for Loading JSON from github, including checks and error handling
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true); // Set loading to true initially
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const jsonURL =
+      "https://raw.githubusercontent.com/kevbrnen/Cs385Project/main/src/jsonFiles/soundFiles.json";
+
+    async function fetchData() {
+      try {
+        const response = await fetch(jsonURL);
+        const json = await response.json();
+        setData(json.soundFiles);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  const selectAudio = (audioUrl) => {
+    props.setAudioUrl(audioUrl);
+    props.setAudioLoaded(false);
+  };
+
+  //Conditional rendering of json file, including buttons to select different audio URLs (need to be included in JSON)
+  //to play from
+  if (error) {
+    return <h1>Oops! An error has occurred: {error.toString()}</h1>;
+  } else if (loading) {
+    return <h1>Loading Data... Please wait!</h1>;
+  } else {
+    return (
+      <>
+        {Array.isArray(data) &&
+          data.map((a, index) => (
+            <p key={index}>
+              <b>{a.title}</b>, <i>{a.environment.location}</i>,{" "}
+              <button onClick={() => selectAudio(a.URL)}> Select </button>
+            </p>
+          ))}
+      </>
+    );
+  }
 }
